b10be81f3dbd170c42eea661b15c8d378fadb4ff
Thomas Witt <tom.89@gmx.at>
2020-08-27 15:01:18 +0200

    change print to printf and use proper format for the date

--- src/AboutMe.java
+++ src/AboutMe.java
@@ -1,21 +1,27 @@
+import java.time.LocalDate;
+
 public class AboutMe {
+
     public static void main(String[] args) {
         String lastName = "Witt";
         String firstName = "Thomas";
         int age = 31;
-        String dateOfBirth = "30.03.1989";
+        LocalDate birthDate = LocalDate.of(1989, 3, 30);
         boolean isFemale = false;
         double averageGrade = 2.1;
         boolean isMarried = false;
 
-        if (isFemale == true) {
+        if (isFemale) {
             System.out.print("Frau ");
         } else {
             System.out.print("Herr ");
         }
-        System.out.print(lastName + " " + firstName + ", der am " + dateOfBirth + " geboren wurde und daher " + age + " Jahre alt ist, ist ");
+        // System.out.print(lastName + " " + firstName + ", der am " + dateOfBirth + " geboren wurde und daher " + age + " Jahre alt ist, ist ");          // My first attempt. Did work but was not pretty
+        // System.out.printf("%s %s%s %s %s %d %s", lastName, firstName, ", der am", dateOfBirth, "geboren wurde und daher", age, "Jahre alt ist, ist ");  // My second attempt. Better, but still kind of ugly
+        System.out.printf("%s %s, der %s geboren wurde und daher %d Jahre alt ist, ist ", firstName, lastName, birthDate, age);
 
-        if (isMarried == false) {
+
+        if (!isMarried) {
             System.out.print("nicht ");
         }
 
