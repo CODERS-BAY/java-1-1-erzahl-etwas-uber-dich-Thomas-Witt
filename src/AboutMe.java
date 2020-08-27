9ea24f065c159a2677d08b5531ad6121bb62d8cb
Thomas Witt <tom.89@gmx.at>
2020-08-27 09:35:21 +0200

    creat the variables and started

---
+++ src/AboutMe.java
@@ -0,0 +1,20 @@
+public class AboutMe {
+    public static void main(String[] args) {
+        String lastName = "Witt";
+        String firstName = "Thomas";
+        int age = 31;
+        String dateOfBirth = "30.03.1989";
+        boolean isFemale = false;
+        double averageGrade = 2.1;
+        boolean isMarried = false;
+
+        if (isFemale = true) {
+            System.out.print("Frau ");
+        } else {
+            System.out.print("Herr ");
+        }
+        System.out.println(lastName + " " + firstName + ", der am " + dateOfBirth + " geboren wurde und daher " + age + " Jahre alt ist");
+
+
+    }
+}
